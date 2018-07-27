9bd5e9b743d8ae3997d87dfe434d518596bcb75b
qpjoy2018 <qpjoy2018@163.com>
2018-07-27 20:33:44 +0800

    init animations

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('myAwesomeAnimation', [
+      state('small', style({
+        transform: 'scale(1)'
+      })),
+      state('large', style({
+        transform: 'scale(1.2)'
+      })),
+      transition('small <=> large', animate('300ms ease-in'
+      //   , style({
+      //   transform: 'translateY(40px)'
+      // })
+        , keyframes([
+          style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+          style({opacity: 1, transform: 'translateY(35px)', offset: .5}),
+          style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+        ])
+      )),
+    ])
+  ]
+})
+export class AppComponent {
+  title = 'app';
+  state: any = 'small';
+  animateMe() {
+    this.state = ((this.state === 'small') ? 'large' : 'small');
+  }
+}
